a88676af7d8c57ceb58921c200093d19cf765237
Gustavo Diaz Galeas <diazg.gustavoa@gmail.com>
2025-05-10 11:36:51 -0400

    Fixed project name in copyright header

--- TheMists.Sdk/Models/Commerce/Exchange.cs
+++ TheMists.Sdk/Models/Commerce/Exchange.cs
@@ -1,5 +1,5 @@
 // Copyright (c) 2025 Gustavo Diaz Galeas
-// This file is part of LeyLines.NET SDK.
+// This file is part of TheMists.NET SDK.
 //
 // Licensed under the GNU AGPL v3.0 (see LICENSE file).
 // Additional restriction: This software may not be used for commercial purposes
