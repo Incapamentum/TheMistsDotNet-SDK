0859d52c48077b5fc590c8819838bbaa7ef21072
Gustavo Diaz Galeas <diazg.gustavoa@gmail.com>
2025-05-10 11:24:02 -0400

    Included copyright header

--- TheMists.Sdk/Models/Commerce/Prices.cs
+++ TheMists.Sdk/Models/Commerce/Prices.cs
@@ -1,4 +1,11 @@
-﻿using System.Text.Json.Serialization;
+﻿// Copyright (c) 2025 Gustavo Diaz Galeas
+// This file is part of TheMists.NET SDK.
+//
+// Licensed under the GNU AGPL v3.0 (see LICENSE file).
+// Additional restriction: This software may not be used for commercial purposes
+// without prior written permission from the author.
+
+using System.Text.Json.Serialization;
 
 namespace TheMists.Sdk.Models.Commerce
 {
