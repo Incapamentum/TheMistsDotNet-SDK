f08ab22d592c7ed80f66f456d9ba1366975cc5ad
Gustavo Diaz Galeas <diazg.gustavoa@gmail.com>
2025-05-10 11:22:42 -0400

    Included JSON doc model - Prices

---
+++ TheMists.Sdk/Models/Commerce/Prices.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TheMists.Sdk.Models.Commerce
+{
+    public class PriceEntry
+    {
+        [JsonPropertyName("quantity")]
+        public int Quantity { get; set; }
+
+        [JsonPropertyName("unit_price")]
+        public int UnitPrice { get; set; }
+    }
+
+    public class Prices
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("buys")]
+        public required PriceEntry Buys { get; set; }
+
+        [JsonPropertyName("sells")]
+        public required PriceEntry Sells { get; set; }
+    }
+}
