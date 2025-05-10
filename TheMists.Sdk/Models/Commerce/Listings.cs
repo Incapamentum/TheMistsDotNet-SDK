3b924bcc07c86303eeb36d907d7b58254a76209a
Gustavo Diaz Galeas <diazg.gustavoa@gmail.com>
2025-05-10 11:28:07 -0400

    Included JSON doc model - Listings

---
+++ TheMists.Sdk/Models/Commerce/Listings.cs
@@ -0,0 +1,35 @@
+// Copyright (c) 2025 Gustavo Diaz Galeas
+// This file is part of LeyLines.NET SDK.
+//
+// Licensed under the GNU AGPL v3.0 (see LICENSE file).
+// Additional restriction: This software may not be used for commercial purposes
+// without prior written permission from the author.
+
+using System.Text.Json.Serialization;
+
+namespace TheMists.Sdk.Models.Commerce
+{
+    public class Listing
+    {
+        [JsonPropertyName("listings")]
+        public int Listings { get; set; }
+
+        [JsonPropertyName("unit_price")]
+        public int UnitPrice { get; set; }
+
+        [JsonPropertyName("quantity")]
+        public int Quantity { get; set; }
+    }
+
+    public class ListingData
+    {
+        [JsonPropertyName("id")]
+        public int ItemId { get; set; }
+
+        [JsonPropertyName("buys")]
+        public List<Listing> Buys { get; set; }
+
+        [JsonPropertyName("sells")]
+        public List<Listing> Sells { get; set; }
+    }
+}
