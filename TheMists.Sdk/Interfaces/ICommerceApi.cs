f9734b3902ba4e7eb0856c96446ed8d6e8542059
Gustavo Diaz Galeas <diazg.gustavoa@gmail.com>
2025-05-24 10:47:37 -0400

    Introduction of public-facing interface.

---
+++ TheMists.Sdk/Interfaces/ICommerceApi.cs
@@ -0,0 +1,21 @@
+// Copyright (c) 2025 Gustavo Diaz Galeas
+// This file is part of TheMists.NET SDK.
+//
+// Licensed under the GNU AGPL v3.0 (see LICENSE file).
+// Additional restriction: This software may not be used for commercial purposes
+// without prior written permission from the author.
+
+using TheMists.Sdk.Models.Commerce;
+
+namespace TheMists.Sdk.Interfaces
+{
+    public interface ICommerceApi
+    {
+        Task<List<int>?> GetAllPriceItemIdsAsync();
+        Task<Prices?> GetItemPriceAsync(int id);
+        Task<List<Prices>?> GetItemsPricesAsync(List<int> ids);
+        Task<List<int>?> GetAllListingItemIdsAsync();
+        Task<Listing?> GetItemListingAsync(int id);
+        Task<List<Listing>?> GetItemsListingAsync(List<int> ids);
+    }
+}
